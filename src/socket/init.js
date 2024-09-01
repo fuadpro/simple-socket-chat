402872a85412a57370e514ab6177e1229889ecc1
Fuad Memmedov <m.fuad@code.edu.az>
2024-09-02 03:08:32 +0400

    count online

--- src/socket/init.js
+++ src/socket/init.js
@@ -5,9 +5,19 @@ const socketMessage = require("./message");
 function initSocket(httpServer) {
   const io = new Server(httpServer);
 
+  let connectedClients = 0; 
+
   io.on("connection", (socket) => {
+    connectedClients++;
+    io.emit("update-client-count", connectedClients);  // Broadcast the count to all clients
+    
     socketAuth(socket);
     socketMessage(socket, io);
+
+    socket.on("disconnect", () => {
+      connectedClients--;
+      io.emit("update-client-count", connectedClients);  // Broadcast the updated count to all clients
+    });
   });
 }
 
